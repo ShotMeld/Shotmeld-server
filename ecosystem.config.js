f888abb7258ed833613c753a859e1625ac40cfff
Seeridia <seeridia@gmail.com>
2025-05-12 15:08:18 +0800

    perf: 更小内存占用

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [{
     name: "shotmeld-server",
     script: "index.js",
-    watch: true,
+    watch: false,
     max_memory_restart: "512M",
     kill_timeout: 3000,
     wait_ready: false,
     listen_timeout: 50000,
     env: {
-      NODE_ENV: "production",
+      NODE_ENV: "production"
     }
   }]
 }
