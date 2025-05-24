ee7960dd61b2cbbf3d53f61b98ca5264b15c04eb
Seeridia <seeridia@gmail.com>
2025-05-25 00:38:49 +0800

    feat: 添加日志配置，集群模式运行提高稳定性

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,22 @@ module.exports = {
     kill_timeout: 3000,
     wait_ready: false,
     listen_timeout: 50000,
+    instances: "max",
+    exec_mode: "cluster",
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 4000,
+    error_file: "logs/error.log",
+    out_file: "logs/out.log",
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    merge_logs: true,
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      PORT: 3000
+    },
+    env_development: {
+      NODE_ENV: "development",
+      PORT: 3000
     }
   }]
 }
